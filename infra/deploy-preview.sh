9711de5ccf9974f760f3bc652d4089c6065c60a9
Patrick Quinn-Graham <7331+thepatrick@users.noreply.github.com>
2022-12-20 16:52:18 +1100

    Add listSurfaces

---
+++ infra/deploy-preview.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+export AWS_DEFAULT_PROFILE=Patrick-Quinn-Graham.AdministratorAccess
+export AWS_PROFILE=Patrick-Quinn-Graham.AdministratorAccess
+
+pulumi up --stack dev
--- infra/src/routes/handlers/registerScreen.ts
+++ infra/src/routes/handlers/registerScreen.ts
@@ -58,13 +58,61 @@ export const registerSurface = surfaceTable.name.apply((tableName) =>
   )
 );
 
-export const listSurfaces = mkLambda(
-  "listSurfaces",
-  async (ev, ctx) => {
-    return {
-      statusCode: 200,
-      body: JSON.stringify(ev)
-    };
-  },
-  lambdaRole
+export const listSurfaces = surfaceTable.name.apply((tableName) =>
+  mkLambda(
+    "listSurfaces",
+    async (ev, ctx) => {
+      const owner = ev.requestContext.authorizer.jwt.claims.sub;
+
+      if (typeof owner !== "string") {
+        return {
+          statusCode: 403,
+          headers: {
+            "content-type": "application/json"
+          },
+          body: JSON.stringify({ error: "Nope" })
+        };
+      }
+
+      (getSegment() as Segment).setUser(`${owner}`);
+
+      const dynamo = captureAWSClient(new DynamoDB());
+
+      try {
+        const items = await dynamo
+          .query({
+            TableName: tableName,
+            KeyConditionExpression: "Owner = :owner",
+            ExpressionAttributeValues: {
+              owner: {
+                S: owner
+              }
+            }
+          })
+          .promise();
+
+        const returnable = items.Items?.map((item) =>
+          DynamoDB.Converter.unmarshall(item)
+        );
+
+        return {
+          statusCode: 200,
+          headers: {
+            "content-type": "application/json"
+          },
+          body: JSON.stringify({ items: returnable })
+        };
+      } catch (error) {
+        console.log("Error", error);
+        return {
+          statusCode: 200,
+          headers: {
+            "content-type": "application/json"
+          },
+          body: JSON.stringify({ error: "Nope" })
+        };
+      }
+    },
+    lambdaRole
+  )
 );
